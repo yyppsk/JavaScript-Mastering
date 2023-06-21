971d5713becbb87838467aa6b58bd82225f8963c
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-06-22 01:25:23 +0530

    Assignment 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,12 +1,1 @@
-/*
-LECTURE: Values and Variables
-1. Declare variables called 'country', 'continent' and 'population' and
-assign their values according to your own country (population in millions)
-2. Log their values to the console
-*/
-let country = "India";
-let continent = "Asia";
-let population = "1,406,000,000";
-console.log(country);
-console.log(continent);
-console.log(population);
+
