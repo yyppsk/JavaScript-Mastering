9ba3afe4d19be5dc3662d6afb260466911f7300e
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-06-22 01:45:37 +0530

    Data Types in JavaScript

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,1 +1,6 @@
-
+let year = 2023;
+console.log(year);
+console.log("Type of year : ", typeof year);
+year = "Two thousand twenty three";
+console.log(year);
+console.log("Type of year : ", typeof year);
