645dc9094f06aa516d795e9b4f157e4fd4af0d77
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-06-24 03:02:52 +0530

    17. Strings and Template Literals

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -5,7 +5,7 @@ console.log("Type of year : ", typeof year);
 year = "Two thousand twenty three";
 console.log(year);
 console.log("Type of year : ", typeof year);
-*/
+
 const now = 2023;
 const agePranjal = now - 1999;
 console.log(agePranjal);
@@ -14,3 +14,9 @@ const firstName = "Pranjal";
 const lastName = "Singh";
 const middleName = "Pratap";
 console.log(firstName + " " + middleName + " " + lastName);
+*/
+
+const pranjalDob = 1999;
+const myname = "Pranjal";
+
+console.log(`${myname}'s age is ${2023 - pranjalDob} `);
