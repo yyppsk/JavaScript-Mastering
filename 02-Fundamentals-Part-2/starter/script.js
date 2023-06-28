bfc1a1b1a9531d9e5e1a9cc8f2a3e430c7be3a7d
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-06-29 03:11:53 +0530

    34. Function Declarations vs. Expressions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,10 +1,24 @@
 //32. Activating Strict Mode
 "use strict";
 /*
- */
+
 function fruitProcessor(apples, oranges) {
   console.log(apples, oranges);
   const juice = `Juice with ${apples} applea and ${oranges} oranges.`;
   return juice;
 }
 console.log(fruitProcessor(5, 2));
+ */
+
+//Function Declaration
+function calcAge1(birthYear) {
+  return 2023 - birthYear;
+}
+const age1 = calcAge1(1999);
+console.log(age1);
+//Function expression
+const calcAge2 = function (birthYear) {
+  return 2023 - birthYear;
+};
+const age2 = calcAge2(1999);
+console.log(age2);
