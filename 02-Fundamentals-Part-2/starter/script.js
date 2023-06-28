cfd74a73b45b7de2be4377b6ee8c54eee4440b76
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-06-29 03:03:25 +0530

    33. Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,2 +1,10 @@
 //32. Activating Strict Mode
 "use strict";
+/*
+ */
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} applea and ${oranges} oranges.`;
+  return juice;
+}
+console.log(fruitProcessor(5, 2));
