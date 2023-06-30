169ce3bfdd01e0ecd811c5c5d3456fed77cad713
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-07-01 01:57:42 +0530

    62. Coding Challenge #1

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -30,3 +30,13 @@ const measureKelvin = function () {
 
 console.log(measureKelvin());
 */
+const printForecast = function (arr) {
+  //to print
+  //"... 17*C in 1 days ... 21*C in 2 days ... 23* C in 3 days ..."
+  let result = "";
+  for (let i = 0; i < arr.length; i++) {
+    result = result + `${arr[i]}*C in ${i + 1} days ...`;
+  }
+  return result;
+};
+console.log("..." + printForecast([17, 21, 23]));
