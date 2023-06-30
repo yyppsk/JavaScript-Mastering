c22358d5f671d68cca3955f1af9a46c8f74b6b9e
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-07-01 01:49:39 +0530

    61. Debugging with the Console and Breakpoints

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,32 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
+/*
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const currTemp = temps[i];
+    if (typeof currTemp !== "number") continue;
+    if (currTemp > max) max = currTemp;
+    if (currTemp < min) min = currTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const tempuratures = [12, 14, "error", 24, 17, 36];
+const amplitude = calcTempAmplitude(tempuratures);
+console.log(amplitude);
 
+const measureKelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "celsius",
+    value: Number(prompt("Degree celcius:")),
+  };
+  console.table(measurement);
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+console.log(measureKelvin());
+*/
