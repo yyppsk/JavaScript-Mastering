951ded797390a95e424ad258f6ffee0283cb4c50
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-07-01 23:25:03 +0530

    76. Coding Challenge #1

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -5,9 +5,20 @@
 // document.querySelector('.score').textContent = 10;
 // document.querySelector('.guess').value = 23;
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('.guess').value = '';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('body').style.backgroundColor = '#222';
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.number').textContent = '?';
+});
+
 document.querySelector('.check').addEventListener('click', function () {
   //console.log(document.querySelector('.guess').value);
   const guess = Number(document.querySelector('.guess').value);
@@ -16,7 +27,7 @@ document.querySelector('.check').addEventListener('click', function () {
   }
   //Player Wins
   else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct Number! 🎉';
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
   }
