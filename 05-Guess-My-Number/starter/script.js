f0b49849126c7de6a7aa920f75266ecfe734016c
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-07-01 13:50:51 +0530

    75. Manipulating CSS Styles

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -13,14 +13,22 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number!';
-  } else if (guess === secretNumber)
+  }
+  //Player Wins
+  else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number! 🎉';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+  }
+  //Too high
   else if (guess > secretNumber) {
     if (score > 1) {
       score--;
       document.querySelector('.message').textContent = 'Too High 📈';
       document.querySelector('.score').textContent = score;
-    } else {
+    }
+    //Too low
+    else {
       document.querySelector('.message').textContent = 'You lost the game!💔';
       document.querySelector('.score').textContent = 0;
     }
