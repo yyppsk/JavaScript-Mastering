de8ba50c3292cfadf84f483f46be5c2f09279229
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-07-01 23:31:29 +0530

    77. Implementing Highscores

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -7,7 +7,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
+let highScore = 0;
 document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.guess').value = '';
   document.querySelector('.message').textContent = 'Start guessing...';
@@ -27,9 +27,15 @@ document.querySelector('.check').addEventListener('click', function () {
   }
   //Player Wins
   else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
   }
   //Too high
   else if (guess > secretNumber) {
