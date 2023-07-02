1c418345344d40de2a110b13477ddf822db614b1
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-07-03 00:35:09 +0530

    82. PROJECT #3: Pig Game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,8 @@
 'use strict';
+// Selecting score elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
