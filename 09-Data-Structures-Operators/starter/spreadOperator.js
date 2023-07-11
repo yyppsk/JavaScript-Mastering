df6614b2cbf81eabd1c1f46f472c341640d450bc
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-07-12 01:47:31 +0530

    105. The Spread Operator (...)

---
+++ 09-Data-Structures-Operators/starter/spreadOperator.js
@@ -0,0 +1,77 @@
+'use strict';
+//104. Destructuring Objects
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order recieved ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} AT ${time} `
+    );
+  },
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your pasta with ${ing1}, ${ing2} and ${ing3}`);
+  },
+};
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+
+//Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+//Join 2 arrays
+const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(menu);
+
+//Iterables are : arrays, strings, maps, sets
+const name = 'pranjal';
+const letters = [...name, ' ', 'S.'];
+console.log(letters);
+// error stuff as multiple arguments console.log(`${...name}`)
+/*
+const ingredients = [
+  prompt('Ingridient 1 ?'),
+  prompt('Ingridient 2 ?'),
+  prompt('Ingridient 3 ?'),
+];
+restaurant.orderPasta(...ingredients);
+*/
+
+//Iterating on objects , new rest
+const newRest = { foundedIn: 1998, ...restaurant, founder: 'Linguini' };
+console.log(newRest);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Ristorante Roma';
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
