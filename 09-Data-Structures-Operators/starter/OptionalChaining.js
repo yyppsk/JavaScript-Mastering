2c5748350e7ea1ccf5956a6ad4f9983a9d2a2188
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-07-21 01:54:36 +0530

    113. Optional Chaining (?.)

---
+++ 09-Data-Structures-Operators/starter/OptionalChaining.js
@@ -0,0 +1,45 @@
+'use strict';
+const openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  openingHours,
+};
+
+//console.log(restaurant.openingHours?.open);
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed'; //nullish coalescing
+  console.log(open);
+}
+//methods if exist or not then call it
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+//optional chaining works on array too
+const users = [
+  {
+    name: 'jonas',
+    email: 'stuff@gmail.com',
+  },
+];
+console.log(users[0]?.name ?? 'User array empty');
---
+++ 09-Data-Structures-Operators/starter/enhancedObjectLiterals.js
@@ -0,0 +1,41 @@
+'use strict';
+const openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order recieved ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} AT ${time} `
+    );
+  },
+  //ES6 enhanced object literal
+  openingHours,
+  //A crisp way to declare function in ES6
+  order(starterIndex, mainIndex) {
+    return 'stuff';
+  },
+};
