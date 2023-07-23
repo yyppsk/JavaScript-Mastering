2d616bef14c815b2fa6c4bacc272b6f0a73b135f
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-07-24 00:39:06 +0530

    118. Maps: Iteration

--- 09-Data-Structures-Operators/starter/SetsAndMaps.js
+++ 09-Data-Structures-Operators/starter/SetsAndMaps.js
@@ -1,4 +1,18 @@
 'use strict';
+const openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
 
 const orderSet = new Set(['Pasta', 'Pasta', 'Risotto', 'Pizza', 'Pizza']);
 console.log(orderSet);
@@ -42,3 +56,33 @@ console.log(rest.has('categories'));
 const arr = [1, 2];
 rest.set(arr, 'Test'); // it will not be retrieved directly this has different object memory
 console.log(rest.get(arr));
+
+const question = new Map([
+  ['question', 'Best language in the world?'],
+  [1, 'C'],
+  [2, 'JAVA'],
+  [3, 'Python'],
+  [4, 'JavaScript'],
+  ['correct', 4],
+  [true, 'Correct 💦'],
+  [false, 'Try again'],
+]);
+console.log(question);
+
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+//already an existing object to Map
+
+//iteration
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+//const answer = Number(prompt('Your answer'));
+//console.log(question.get(question.get('correct') === answer));
+//MAP TO ARRAY CONVERSION
+
+console.log(...question);
+console.log(...question.entries());
+console.log([...question.values()]);
+console.log([...question.keys()]);
