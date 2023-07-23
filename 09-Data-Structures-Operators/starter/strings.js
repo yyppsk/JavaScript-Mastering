e5c91e403b78ea895d63881a2bb26d9641d70662
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-07-24 01:51:41 +0530

    122. Working With Strings - Part 2

--- 09-Data-Structures-Operators/starter/strings.js
+++ 09-Data-Structures-Operators/starter/strings.js
@@ -24,3 +24,50 @@ const checkMiddleSeat = function (seat) {
 checkMiddleSeat('11B');
 checkMiddleSeat('23C');
 checkMiddleSeat('3E');
+
+//working with cases
+
+console.log(airline.toLowerCase());
+
+//fix passenger name
+const passenger = 'jOnAS';
+const passengerLow = passenger.toLowerCase();
+const passengerCorrect = passenger[0].toUpperCase() + passengerLow.slice(1);
+console.log(passengerLow);
+console.log(passengerCorrect);
+
+//comparing mails
+const loginEmail = 'Hello@Jonas.IO';
+const normalisedEmail = loginEmail.toLowerCase().trim();
+console.log(normalisedEmail);
+
+const textReplace = '989,244';
+console.log(textReplace.replace(',', '.'));
+
+//RegEX
+const announcement =
+  'All passengers come to the boarding door 23. Boarding door 23!';
+console.log(announcement.replace(/door/g, 'gate'));
+
+//Booleans
+const plane2 = 'Airbus A320neo';
+console.log(plane2.includes('A320'));
+console.log(plane2.startsWith('Airb'));
+
+if (plane2.startsWith('Airbus') && plane2.endsWith('neo')) {
+  console.log('New');
+}
+
+//practice
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+  if (baggage.includes('knife') || baggage.includes('gun'))
+    console.log('NOT ALLOWED!!');
+  else {
+    console.log('Welcome aboard');
+  }
+};
+
+checkBaggage('I have a laptop, some Food and a pocket Knife');
+checkBaggage('Sock and camera');
+checkBaggage('Got some snacks and a gun for protection');
