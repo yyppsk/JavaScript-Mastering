9e596d1978ace944041c2e8124bfd754976d674f
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-07-25 19:02:23 +0530

    125. String Methods Practice

--- 09-Data-Structures-Operators/starter/strings.js
+++ 09-Data-Structures-Operators/starter/strings.js
@@ -113,3 +113,26 @@ console.log(maskCreditCard('333535888199'));
 
 const message2 = 'Bad weather... All Departures Delayed...';
 console.log(message2.repeat(5));
+
+///////////////////////////////////////
+// String Methods Practice
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} from ${from.slice(0, 3).toUpperCase()} to ${to
+    .slice(0, 3)
+    .toUpperCase()} (${time.replace(':', 'h')})`.padStart(36);
+  console.log(output);
+}
