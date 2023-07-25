25d1b2a4e4a2d3ac138182764768a3f9711843d0
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-07-25 22:07:08 +0530

    128. Default Parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+const bookings = [];
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  //es 6 ways
+  //ES 5 way ugly way
+  //numPassengers = numPassengers || 1; //falsy values results to true value
+  //price = price || 199;
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 5);
