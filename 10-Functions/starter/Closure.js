ca7b51077e310ad7bebc805a72ebc2d30ca64045
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-08-06 03:00:46 +0530

    Closures

---
+++ 10-Functions/starter/Closure.js
@@ -0,0 +1,48 @@
+'use strict';
+const secureBooking = function () {
+  let passengerCount = 0;
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+const booker = secureBooking();
+booker();
+booker();
+booker();
+console.dir(booker);
+
+//138. More on closures
+
+let f;
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+g();
+f();
+//reassign f
+h();
+f();
+
+//Example 2
+const boardPassengers = function (n, wait) {
+  //const perGroup = n / 3;
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+  console.log(`Will start boarding in ${wait}`);
+};
+const perGroup = 1000;
+boardPassengers(180, 3);
+console.dir(boardPassengers);
